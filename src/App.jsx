98e7f179d5c85a2bd67d2ea1cd1db2fc836a8ab5
Niloofar Nayebi <nil_nyb@yahoo.com>
2026-05-05 21:58:21 -0400

    week-05 completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,25 @@ function App() {
     const newTodo = {
       id: Date.now(),
       title: todoTitle,
+      isCompleted: false,
     };
 
     setTodoList((previous) => [newTodo, ...previous]);
+  }
+
+  function completeTodo(id) {
+    setTodoList((previous) =>
+      previous.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: true } : todo
+      )
+    );
   }
 
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   );
 }
